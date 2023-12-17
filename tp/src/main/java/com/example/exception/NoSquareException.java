a288126a7660edf2d0180af229165e10e9eadb56
YoucefB001 <ky_benali@esi.dz>
2023-12-17 10:38:52 +0100

    version 1

--- tp/src/main/java/com/example/exception/NoSquareException.java
+++ tp/src/main/java/com/example/exception/NoSquareException.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.example.exception;
 
 public class NoSquareException extends Exception {
@@ -15,21 +14,3 @@ public class NoSquareException extends Exception {
 	}
 
 }
-=======
-package com.example.exception;
-
-public class NoSquareException extends Exception {
-
-	public NoSquareException() {
-		super();
-		// TODO Auto-generated constructor stub
-	
-	}
-
-	public NoSquareException(String message) {
-		super(message);
-		// TODO Auto-generated constructor stub
-	}
-
-}
->>>>>>> origin/dev2
